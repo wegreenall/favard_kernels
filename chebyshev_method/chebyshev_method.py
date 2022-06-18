1ea3aa9b34a62bbddeba31161cf5022852dba93d
wegreenall <wjgreenall@gmail.com>
2022-06-18 08:46:59 +0300

    about to work on fixing the jordan matrix bit

--- chebyshev_method/chebyshev_method.py
+++ chebyshev_method/chebyshev_method.py
@@ -5,7 +5,7 @@ from termcolor import colored
 import matplotlib.pyplot as plt
 
 torch.set_default_tensor_type(torch.DoubleTensor)
-torch.set_printoptions(precision=10, linewidth=200)
+torch.set_printoptions(precision=5, linewidth=300)
 
 """
 The Gautschi technique via the Chebyshev method appears to have a problem of 
@@ -33,7 +33,6 @@ def weight_mask(order) -> (torch.Tensor, torch.Tensor):
 
     # block = torch.vstack((torch.ones(order), s, t)).t()
     for i in range(order + 1):
-        # breakpoint()
         if i < order - 2:
             matrix[i, i : i + 3] = torch.tensor([zeros[i], ones[i], ones[i]])
             matrix_2[i, i : i + 3] = torch.tensor(
@@ -58,7 +57,6 @@ def jordan_matrix(s, t):
     matrix = torch.zeros((order + 1, order))
     block = torch.vstack((torch.ones(order), s, t)).t()
     for i in range(order + 1):
-        # breakpoint()
         if i < order - 2:
             matrix[i, i : i + 3] = torch.tensor([ones[i], s[i], t[i]])
         elif i < order - 1:
@@ -66,8 +64,9 @@ def jordan_matrix(s, t):
         elif i < order:
             matrix[i, i] = torch.tensor([ones[i]])
     # for i
-    # print(matrix)
-    # breakpoint()
+    print("JORDAN MATRIX:")
+    print(colored(matrix.t(), "red"))
+    breakpoint()
     return matrix[:, :].t()
 
 
@@ -91,14 +90,21 @@ class CatNet(nn.Module):
         self.mask_matrix = jordan_matrix(
             torch.ones(2 * order), torch.ones(2 * order)
         )
-        breakpoint()
+        # self.mask_matrix[:, 0] = torch.zeros(2 * order)
+        # self.mask_matrix[0, 0] = 1.0
+        # self.mask_matrix[1, 0] = 0.0
+        print(colored(self.mask_matrix, "yellow"))
+        print("\n")
         self.layers = []
 
         for i in range(1, 2 * order + 1):
             layer = nn.Linear(i, i + 1, bias=False)
+            breakpoint()
             layer.weight = torch.nn.Parameter(
                 self.jordan[: i + 1, :i].view(-1, i)
             )
+            print("This layer", layer)
+            print("This layer weight:", layer.weight)
             self.layers.append(layer)
         return
 
@@ -140,15 +146,19 @@ class CatNet(nn.Module):
 
 if __name__ == "__main__":
     order = 6
-    opt_norm = False
+    opt_norm = True
+    opt_symmetric = True
     # betas = torch.zeros(2 * order)
-    betas = torch.ones(2 * order)
-    gammas = torch.ones(2 * order)
+    betas = torch.zeros(2 * order)
+    gammas = torch.linspace(1, 2 * order, 2 * order) / 2  # 2 * order)
     betas.requires_grad = True
     gammas.requires_grad = True
     cat_net = CatNet(order, betas, gammas)
     value = cat_net(torch.Tensor([1.0]))
-    print(value)
+    print(colored("Given initial betas", "blue"), colored(betas, "yellow"))
+    print(colored("Given initial gammas", "blue"), colored(gammas, "yellow"))
+    print("True moments for given initial betas:", value)
+    breakpoint()
 
     initial_jordan = jordan_matrix(betas, gammas)
     correct_jordan = jordan_matrix(
@@ -157,13 +167,34 @@ if __name__ == "__main__":
     for i in range(1, 2 * order + 1):
         weights = initial_jordan[: i + 1, :i].view(-1, i)
         # print(weights)
-    true_moments = torch.Tensor(
-        [1.0, 0.0, 1.0, 0.0, 2.0, 0.0, 5.0, 0.0, 14.0, 0.0]
-    )
+    if opt_symmetric:
+        true_betas = torch.zeros(order)
+        true_gammas = torch.ones(order)
+        true_moments = torch.Tensor(
+            [1.0, 0.0, 1.0, 0.0, 2.0, 0.0, 5.0, 0.0, 14.0, 0.0, 42.0, 0.0]
+        )
+    else:
+        true_betas = torch.ones(order)
+        true_gammas = torch.ones(order)
+        true_moments = torch.Tensor(
+            [
+                1.0,
+                1.0,
+                2.0,
+                4.0,
+                9.0,
+                21.0,
+                51.0,
+                127.0,
+                323.0,
+                835.0,
+                2188.0,
+                5798.0,
+            ]
+        )
     start_moments = torch.zeros(true_moments.shape)
 
     # set up optimisation
-    breakpoint()
 
     params_list = []
     for layer in cat_net.layers:
@@ -172,38 +203,46 @@ if __name__ == "__main__":
 
     if opt_norm:
         # optimiser = torch.optim.Adam(params_list, lr=0.000005)
-        optimiser = torch.optim.Adamax(params_list, lr=0.00005)
+        # optimiser = torch.optim.Adamax(params_list, lr=0.00005)
 
-        # optimiser = torch.optim.SGD(params_list, lr=0.00005)
+        optimiser = torch.optim.SGD(params_list, lr=0.00005)
     else:
-        optimiser = torch.optim.Adamax(params_list, lr=0.00005)
-        # optimiser = torch.optim.Adam(params_list, lr=0.000005)
+        # optimiser = torch.optim.Adamax(params_list, lr=0.00005)
+        # optimiser = torch.optim.Adam(params_list, lr=0.00005)
 
-        # optimiser = torch.optim.SGD(params_list, lr=0.00005)
+        optimiser = torch.optim.SGD(params_list, lr=0.00005)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimiser, gamma=0.99)
 
     iterations = 0
+    # breakpoint()
     while not torch.allclose(start_moments, true_moments, atol=1e-13):
         optimiser.zero_grad()
         start_moments = cat_net(torch.Tensor([1.0]))
         if opt_norm:
-            loss = torch.norm(true_moments - start_moments, 2)
+            # breakpoint()
+            loss = torch.norm(true_moments - start_moments, 3)
             loss.backward()
         else:
-            loss = torch.pow(true_moments - start_moments, 2)
+            # breakpoint()
+            loss = torch.pow(torch.abs(true_moments - start_moments), 2)
             loss.backward(torch.ones(2 * order))
-        # with torch.no_grad():
-        # cat_net.
+
         optimiser.step()
         with torch.no_grad():
             cat_net.mask_jordan()
-        if iterations % 500 == 0:
-            print(true_moments - start_moments)
+        if iterations % 100 == 0:
+            print(colored(true_moments - start_moments, "green"))
             betas_gammas_jordan = [
                 param for param in cat_net.layers[-1].parameters()
             ][0]
-            # breakpoint()
-            print(betas_gammas_jordan - correct_jordan[:, :-1])
+            print(
+                colored("true betas:", "yellow"), colored(true_betas, "yellow")
+            )
+            print(
+                colored("truue gammas:", "blue"), colored(true_gammas, "blue")
+            )
+            # print(betas_gammas_jordan - correct_jordan[:, :-1])
+            print(betas_gammas_jordan)
             # print(betas_gammas_jordan)
         iterations += 1
         if iterations % 50000 == 0:
