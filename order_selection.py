a2693267a3c5f38618f880b198890badba3b540c
wegreenall <wjgreenall@gmail.com>
2023-05-08 12:04:14 +0300

    added order selection file for polynomial eigenvalues, and completed for smooth exponential eigenvalues  # Please enter the commit message for your changes. Lines starting

--- order_selection.py
+++ order_selection.py
@@ -14,10 +14,10 @@ import pickle
 # now insert a function that does some stuff
 if __name__ == "__main__":
     torch.manual_seed(0)
-    run_calculations
+    run_calculations = True
     # 1) get the true function: a mercer GP.
     plot_ground_truth = True
-    true_noise_std = 0.2
+    true_noise_std = 0.5
     true_order = 12
 
     parameters = {
@@ -57,45 +57,77 @@ if __name__ == "__main__":
     saved_noises: List = []
     saved_eigenvalues: List = []
     saved_completions: List[bool] = []
+    saved_ards: List = []
+    saved_variances: List = []
+    if run_calculations:
+        # 3) get the likelihood
+        for order in range(3, 25):
+            initial_noise = torch.Tensor([1.0])
+            initial_parameters = {
+                "ard_parameter": torch.Tensor([1.0]),
+                "precision_parameter": torch.Tensor([1.0]),
+                "noise_parameter": initial_noise,
+                "variance_parameter": torch.Tensor([1.0]),
+            }
+            eigenvalue_generator = SmoothExponentialFasshauer(order)
+            eigenvalues = eigenvalue_generator(initial_parameters)
+            basis = bf.Basis(
+                bf.smooth_exponential_basis_fasshauer, 1, order, parameters
+            )
+            kernel = MercerKernel(
+                order, basis, eigenvalues, initial_parameters
+            )
+            likelihood = Likelihood(
+                order,
+                kernel,
+                input_sample,
+                output_sample,
+                eigenvalue_generator,
+                param_learning_rate=0.00001,
+                sigma_learning_rate=0.00001,
+            )
+            trained_noise, trained_parameters = likelihood.fit(
+                initial_noise, initial_parameters, max_iterations=5000
+            )
+            final_eigenvalues = eigenvalue_generator(trained_parameters)
+            saved_noises.append(trained_noise)
+            saved_ards.append(trained_parameters["ard_parameter"])
+            saved_variances.append(trained_parameters["variance_parameter"])
+            saved_eigenvalues.append(final_eigenvalues)
+            saved_completions.append(likelihood.converged)
 
-    # 3) get the likelihood
-    for order in range(3, 18):
-        initial_noise = torch.Tensor([1.0])
-        initial_parameters = {
-            "ard_parameter": torch.Tensor([1.0]),
-            "precision_parameter": torch.Tensor([1.0]),
-            "noise_parameter": initial_noise,
-            "variance_parameter": torch.Tensor([1.0]),
-        }
-        eigenvalue_generator = SmoothExponentialFasshauer(order)
-        eigenvalues = eigenvalue_generator(initial_parameters)
-        basis = bf.Basis(
-            bf.smooth_exponential_basis_fasshauer, 1, order, parameters
-        )
-        kernel = MercerKernel(order, basis, eigenvalues, initial_parameters)
-        likelihood = Likelihood(
-            order,
-            kernel,
-            input_sample,
-            output_sample,
-            eigenvalue_generator,
-            param_learning_rate=0.00001,
-            sigma_learning_rate=0.00001,
-        )
-        trained_noise, trained_parameters = likelihood.fit(
-            initial_noise, initial_parameters
-        )
-        final_eigenvalues = eigenvalue_generator(trained_parameters)
-        saved_noises.append(trained_noise)
-        saved_eigenvalues.append(final_eigenvalues)
-        saved_completions.append(likelihood.converged)
+        print(saved_noises)
+        print(saved_eigenvalues)
 
-    print(saved_noises)
-    print(saved_eigenvalues)
+        # now save the eigenvalues data and the noise data
+        with open("eigenvalues.pkl", "wb") as f:
+            pickle.dump(saved_eigenvalues, f)
 
-    # now save the eigenvalues data and the noise data
-    with open("eigenvalues.pkl", "wb") as f:
-        pickle.dump(saved_eigenvalues, f)
+        with open("noises.pkl", "wb") as f:
+            pickle.dump(saved_noises, f)
 
-    with open("noises.pkl", "wb") as f:
-        pickle.dump(saved_noises, f)
+        # save the ard parameters
+        with open("ards.pkl", "wb") as f:
+            pickle.dump(saved_ards, f)
+
+        # save the variance parameters
+        with open("variances.pkl", "wb") as f:
+            pickle.dump(saved_variances, f)
+
+    else:  # i.e. we are going to be loading the calculated values
+        with open("eigenvalues.pkl", "rb") as f:
+            saved_eigenvalues = pickle.load(f)
+
+        with open("noises.pkl", "rb") as f:
+            saved_noises = pickle.load(f)
+
+        with open("ards.pkl", "rb") as f:
+            saved_ards = pickle.load(f)
+
+        with open("variances.pkl", "rb") as f:
+            saved_variances = pickle.load(f)
+
+        # now plot the noises
+        plt.plot(list(range(3, 18)), saved_noises)
+        plt.plot(list(range(3, 18)), [true_noise_std for _ in range(3, 18)])
+        plt.show()
---
+++ order_selection_convergence/order_selection_polynomial_eigenvalues.py
@@ -0,0 +1,6 @@
+"""
+In here we will add the code for the order selection using polynomial 
+eigenvalues. First however we will need to implement the EigenvalueGenerator
+interface for the PolynomialEigenvalues, to see if it works in the Mercer 
+setting.
+"""
