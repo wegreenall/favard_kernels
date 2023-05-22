193c8926912a05815ceb18e99a8f6508e156c9cc
wegreenall <wjgreenall@gmail.com>
2023-05-22 16:51:09 +0300

    added predictive density

---
+++ predictive_density/analysis.py
@@ -0,0 +1,245 @@
+import torch
+import torch.distributions as D
+import matplotlib.pyplot as plt
+import tikzplotlib
+
+"""
+This script conducts the analysis for the predictive density experiment, which generates the 
+ predictive density for the true GP, Mercer GP and Favard
+GP. Then, it calculates the kl-divergence:
+    - from Mercer GP predictive density to true GP predictive density
+    - from Favard GP predictive density to true GP predictive density
+
+These are stored in a .pt tensor file, with a tensor of shape
+[experiment_count, mixture_count, 2].
+
+experiment_count is 1000; mixture_count is 5.
+
+mixture_count represents the travel from a Normal distribution to an 
+increasingly "mixed" input distribution. One would expect that the 
+KL divergence across mixture_count would increase, but _less_ for the Favard.
+"""
+
+normal_input_kls = torch.load(
+    "./experiment_data/kl_divergences_normal_test_inputs.pt"
+)
+mixture_input_kls = torch.load(
+    "./experiment_data/kl_divergences_mixture_test_inputs.pt"
+)
+
+# print(kls.shape)
+print(normal_input_kls)
+print(mixture_input_kls)
+# results = torch.mean(kls, dim=0)
+# print(results)
+# breakpoint()
+first_plot = False
+second_plot = False
+
+experiment_count = normal_input_kls.shape[0]
+if first_plot:
+    for i in range(experiment_count):
+        graph_0 = normal_input_kls[i, :, 0]
+        graph_1 = normal_input_kls[i, :, 1]
+        # breakpoint()
+        loggraph_0 = torch.mean(torch.log(graph_0))
+        loggraph_1 = torch.mean(torch.log(graph_1))
+        plt.plot(loggraph_0 - loggraph_1)
+        # plt.plot(graph_1 - graph_0)
+        # plt.plot(graph_1)
+    plt.show()
+
+if second_plot:
+    for i in range(experiment_count):
+        graph_0 = mixture_input_kls[i, :, 0]
+        graph_1 = mixture_input_kls[i, :, 1]
+        plt.plot(torch.log(graph_1) - torch.log(graph_0))
+        # plt.plot(graph_1)
+    plt.show()
+
+log_data = False
+save_tikz = False
+end_order = 5
+
+# now show the graphs with the mean
+if log_data:
+    normal_meangraph_0 = torch.mean(
+        torch.log(normal_input_kls[:, :end_order, 0]), dim=0
+    )
+    normal_meangraph_1 = torch.mean(
+        torch.log(normal_input_kls[:, :end_order, 1]), dim=0
+    )
+    normal_lqgraph_0 = torch.quantile(
+        torch.log(normal_input_kls[:, :end_order, 0]), 0.25, dim=0
+    )
+    normal_hqgraph_0 = torch.quantile(
+        torch.log(normal_input_kls[:, :end_order, 0]), 0.75, dim=0
+    )
+    normal_lqgraph_1 = torch.quantile(
+        torch.log(normal_input_kls[:, :end_order, 1]), 0.25, dim=0
+    )
+    normal_hqgraph_1 = torch.quantile(
+        torch.log(normal_input_kls[:, :end_order, 1]), 0.75, dim=0
+    )
+    mixture_meangraph_0 = torch.mean(
+        torch.log(mixture_input_kls[:, :end_order, 0]), dim=0
+    )
+    mixture_meangraph_1 = torch.mean(
+        torch.log(mixture_input_kls[:, :end_order, 1]), dim=0
+    )
+    mixture_lqgraph_0 = torch.quantile(
+        torch.log(mixture_input_kls[:, :end_order, 0]), 0.25, dim=0
+    )
+    mixture_hqgraph_0 = torch.quantile(
+        torch.log(mixture_input_kls[:, :end_order, 0]), 0.75, dim=0
+    )
+    mixture_lqgraph_1 = torch.quantile(
+        torch.log(mixture_input_kls[:, :end_order, 1]), 0.25, dim=0
+    )
+    mixture_hqgraph_1 = torch.quantile(
+        torch.log(mixture_input_kls[:, :end_order, 1]), 0.75, dim=0
+    )
+else:
+    normal_meangraph_0 = torch.mean(normal_input_kls[:, :end_order, 0], dim=0)
+    normal_meangraph_1 = torch.mean(normal_input_kls[:, :end_order, 1], dim=0)
+    normal_lqgraph_0 = torch.quantile(
+        normal_input_kls[:, :end_order, 0], 0.25, dim=0
+    )
+    normal_hqgraph_0 = torch.quantile(
+        normal_input_kls[:, :end_order, 0], 0.75, dim=0
+    )
+    normal_lqgraph_1 = torch.quantile(
+        normal_input_kls[:, :end_order, 1], 0.25, dim=0
+    )
+    normal_hqgraph_1 = torch.quantile(
+        normal_input_kls[:, :end_order, 1], 0.75, dim=0
+    )
+    mixture_meangraph_0 = torch.mean(
+        mixture_input_kls[:, :end_order, 0], dim=0
+    )
+    mixture_meangraph_1 = torch.mean(
+        mixture_input_kls[:, :end_order, 1], dim=0
+    )
+    mixture_lqgraph_0 = torch.quantile(
+        mixture_input_kls[:, :end_order, 0], 0.25, dim=0
+    )
+    mixture_hqgraph_0 = torch.quantile(
+        mixture_input_kls[:, :end_order, 0], 0.75, dim=0
+    )
+    mixture_lqgraph_1 = torch.quantile(
+        mixture_input_kls[:, :end_order, 1], 0.25, dim=0
+    )
+    mixture_hqgraph_1 = torch.quantile(
+        mixture_input_kls[:, :end_order, 1], 0.75, dim=0
+    )
+# normal_meangraph_0 = torch.mean((normal_input_kls[:, :, 0]), dim=0)
+# normal_meangraph_1 = torch.mean((normal_input_kls[:, :, 1]), dim=0)
+
+# normal distributed input test
+
+plt.rcParams["text.usetex"] = True
+# plt.rcParams["figure.figsize"] = (2, 1)
+fig, ax = plt.subplots()
+x = torch.linspace(0, len(normal_meangraph_0), len(normal_meangraph_0))
+
+
+# ax.legend(
+# (
+# "Mean KL divergence with Mercer representation",
+# "Mean KL divergence with Favard representation",
+# )
+# )
+ax.set_xlabel(r"$i$")
+ax.set_ylabel(r"$\mathcal{KL}[p_i||q]$")
+ax.plot(
+    x,
+    normal_meangraph_0,
+    x,
+    normal_meangraph_1,
+)
+ax.plot(
+    x,
+    normal_lqgraph_0,
+    x,
+    normal_hqgraph_0,
+    x,
+    normal_lqgraph_1,
+    x,
+    normal_hqgraph_1,
+    linestyle="--",
+)
+if save_tikz:
+    tikzplotlib.save(
+        "predictivedensitydiagram1.tex",
+        axis_height="\\predictivedensitydiagramheight",
+        axis_width="\\predictivedensitydiagramwidth",
+    )
+else:
+    plt.show()
+
+# plt.savefig(
+# "/home/william/phd/tex_projects/favard_kernels_icml/diagrams/predictivedensitydiagram.eps",
+# format="eps",
+# dpi=1200,
+# )
+fig2, ax2 = plt.subplots()
+ax2.set_xlabel(r"$i$")
+ax2.set_ylabel(r"$\mathcal{KL}[p_i||q]$")
+ax2.plot(
+    x,
+    mixture_meangraph_0,
+    x,
+    mixture_meangraph_1,
+)
+ax2.plot(
+    x,
+    mixture_lqgraph_0,
+    x,
+    mixture_hqgraph_0,
+    x,
+    mixture_lqgraph_1,
+    x,
+    mixture_hqgraph_1,
+    linestyle="--",
+)
+if save_tikz:
+    tikzplotlib.save(
+        "predictivedensitydiagram2.tex",
+        axis_height="\\predictivedensitydiagramheight",
+        axis_width="\\predictivedensitydiagramwidth",
+    )
+else:
+    plt.show()
+# plt.savefig(
+# "/home/william/phd/tex_projects/favard_kernels_icml/diagrams/predictivedensitydiagram2.eps",
+# format="eps",
+# dpi=1200,
+# )
+# plt.savefig(
+# "/home/william/phd/tex_projects/favard_kernels_icml/diagrams/predictivedensitydiagram.svg",
+# format="svg",
+# dpi=1200,
+# )
+# plt.show()
+# ax.set_title()
+
+# plt.plot(normal_meangraph_0.numpy())
+# plt.plot(normal_lqgraph_0.numpy(), linestyle="--")
+# plt.plot(normal_hqgraph_0.numpy(), linestyle="--")
+# plt.plot(normal_meangraph_1.numpy())
+# plt.plot(normal_lqgraph_1.numpy(), linestyle="--")
+# plt.plot(normal_hqgraph_1.numpy(), linestyle="--")
+# # plt.ylabel(r"KL[p_i || q]")
+# # plt.xlabel(r"i")
+# plt.show()
+
+# mixture input test
+# plt.plot(mixture_meangraph_0.numpy())
+# plt.plot(mixture_lqgraph_0.numpy(), linestyle="--")
+# plt.plot(mixture_hqgraph_0.numpy(), linestyle="--")
+# plt.plot(mixture_meangraph_1.numpy())
+# plt.plot(mixture_lqgraph_1.numpy(), linestyle="--")
+# plt.plot(mixture_hqgraph_1.numpy(), linestyle="--")
+# plt.ylabel(r"\mathcal{KL}\left[g_i || q\right]")
+# plt.xlabel(r"i")
+# plt.show()
---
+++ predictive_density/experiment.py
@@ -0,0 +1,297 @@
+import torch
+import torch.distributions as D
+import matplotlib.pyplot as plt
+import math
+
+from ortho.basis_functions import (
+    smooth_exponential_basis_fasshauer,
+    smooth_exponential_eigenvalues_fasshauer,
+    Basis,
+    CompositeBasis,
+    RandomFourierFeatureBasis,
+)
+from ortho.builders import get_orthonormal_basis_from_sample, OrthoBuilder
+
+from mercergp.builders import (
+    train_smooth_exponential_mercer_params,
+    build_smooth_exponential_mercer_gp,
+    train_mercer_params,
+    build_mercer_gp,
+)
+
+from mercergp.MGP import MercerGP
+from mercergp.builders import MercerGPBuilder
+
+# from mercergp.posterior_sampling import NonStationarySpectralDistribution
+from mercergp.kernels import (
+    MercerKernel,
+    RandomFourierFeaturesKernel,
+    SmoothExponentialKernel,
+)
+from mercergp.eigenvalue_gen import SmoothExponentialFasshauer
+import matplotlib.pyplot as plt
+from termcolor import colored
+
+
+def test_function(x: torch.Tensor) -> torch.Tensor:
+    """
+    Test function used in an iteration of Daskalakis, Dellaportas and Panos.
+    """
+    return (1.5 * torch.sin(x) + 0.5 * torch.cos(4 * x) + x / 8).squeeze()
+
+
+def weight_function(input: torch.Tensor):
+    return torch.exp(-(input**2))
+
+
+"""
+This experiment generates the predictive density of:
+    - the true GP with full kernel
+    - the Mercer GP, 
+    - the Favard GP, 
+under the inputs:
+    - Normal(0, 1)
+    - A sequence of ever more "distant" mixture input distributions.
+and compares the KL divergences
+"""
+
+
+def mean_generator(i):
+    return 1 * i
+
+
+def get_normal_density_log(
+    covariance_matrix: torch.Tensor,
+    mean_vector: torch.Tensor,
+    value_vector: torch.Tensor,
+) -> torch.Tensor:
+    k = covariance_matrix.shape[0]
+    term_1 = -k / 2 * math.log(2 * math.pi)
+    term_2 = -0.5 * torch.logdet(covariance_matrix)
+    print("determinant:", torch.exp(-term_2))
+    inverse_matrix = torch.inverse(covariance_matrix)
+    term_3 = -0.5 * (
+        (value_vector - mean_vector).t()
+        @ inverse_matrix
+        @ (value_vector - mean_vector)
+    )
+    return term_1 + term_2 + term_3
+
+
+def get_posterior_mean(
+    kernel: SmoothExponentialKernel,
+    test_points: torch.Tensor,
+    inputs: torch.Tensor,
+    outputs: torch.Tensor,
+):
+    noise_parameter = kernel.get_params()["noise_parameter"]
+    kernel_matrix_inv = kernel.kernel_inverse(inputs)
+    kernel_left = kernel(inputs, test_points)
+    result = kernel_left @ kernel_matrix_inv @ outputs
+    return result.squeeze()
+
+
+def get_predictive_density_se_kernel(
+    kernel: SmoothExponentialKernel,
+    test_points: torch.Tensor,
+    inputs: torch.Tensor,
+    outputs: torch.Tensor,
+) -> D.Distribution:
+    # calculate the mean
+    posterior_predictive_mean = get_posterior_mean(
+        kernel, test_points, inputs, outputs
+    )
+
+    # now calculate the variance
+    posterior_predictive_variance = (
+        kernel(test_points, test_points)
+        - kernel(inputs, test_points)
+        @ kernel.kernel_inverse(inputs)
+        @ kernel(test_points, inputs)
+        # + kernel.kernel_args["noise_parameter"] ** 2
+    )
+
+    # add jitter for positive definiteness
+    # posterior_predictive_variance += 0.00001 * torch.eye(len(test_points))
+
+    posterior_predictive_variance += kernel.kernel_args[
+        "noise_parameter"
+    ] * torch.eye(len(test_points))
+    try:
+        distribution = D.MultivariateNormal(
+            posterior_predictive_mean,
+            posterior_predictive_variance,
+        )
+    except ValueError:
+        distribution = D.MultivariateNormal(
+            posterior_predictive_mean,
+            posterior_predictive_variance,
+        )
+        print("FOUND NEGATIVE VARIANCE!")
+    return distribution
+
+
+if __name__ == "__main__":
+    """
+    The program begins here
+    """
+    normal_test_inputs = False
+    save = True
+    # metaparameters
+    experiment_count = 1000
+    mixtures_count = 6
+
+    # set up the arguments
+    order = 12  # degree of approximation
+    dim = 1
+    sample_size = 4000
+
+    l_se = torch.Tensor([[1]])
+    sigma_se = torch.Tensor([1])
+    sigma_e = torch.Tensor([0.2])
+    epsilon = torch.Tensor([1])
+    kernel_args = {
+        "ard_parameter": l_se,
+        "variance_parameter": sigma_se,
+        "noise_parameter": sigma_e,
+        "precision_parameter": epsilon,
+    }
+    variance = 0.8  # the variance of each of the components of the experimental mixtures.
+    test_inputs_sample_size = 15
+    test_input_sample_shape = torch.Size([test_inputs_sample_size])
+    if normal_test_inputs:
+        test_inputs_distribution = D.Normal(0.0, 1.0)
+    else:
+        mixing_distribution = D.Categorical(torch.Tensor([0.2, 0.8]))
+        component_distribution = D.Normal(
+            torch.Tensor([-mean_generator(2), mean_generator(2)]),
+            torch.Tensor([variance, variance]),
+        )
+        test_inputs_distribution = D.MixtureSameFamily(
+            mixing_distribution, component_distribution
+        )
+
+    se_kernel = SmoothExponentialKernel(kernel_args)
+    kls = torch.zeros(experiment_count, mixtures_count, 2)
+    favard_failures = torch.zeros(experiment_count, mixtures_count)
+    mercer_failures = torch.zeros(experiment_count, mixtures_count)
+    mixing_distribution = D.Categorical(torch.Tensor([0.2, 0.8]))
+    # shared parameters
+    eigenvalues = smooth_exponential_eigenvalues_fasshauer(order, kernel_args)
+    eigenvalue_generator = SmoothExponentialFasshauer(order, 1)
+
+    # Mercer kernel
+    mercer_basis = Basis(
+        smooth_exponential_basis_fasshauer, 1, order, kernel_args
+    )
+    mercer_gp_builder = (
+        MercerGPBuilder(order)
+        .set_parameters(kernel_args)
+        .set_basis(mercer_basis)
+        .set_eigenvalue_generator(eigenvalue_generator)
+    )
+    favard_gp_builder = (
+        MercerGPBuilder(order)
+        .set_parameters(kernel_args)
+        .set_eigenvalue_generator(eigenvalue_generator)
+    )
+    for i in range(experiment_count):
+        for j in range(mixtures_count):
+            print("Experiment {} in progress!".format((str(i), str(j))))
+            test_inputs_sample = test_inputs_distribution.sample(
+                test_input_sample_shape
+            )
+
+            # make the inputs
+            mean = mean_generator(j)
+
+            # input data
+            component_distribution = D.Normal(
+                torch.Tensor([-mean, mean]), torch.Tensor([variance, variance])
+            )
+            dist = D.MixtureSameFamily(
+                mixing_distribution, component_distribution
+            )
+
+            inputs = dist.sample([sample_size])
+            outputs = test_function(inputs) + torch.distributions.Normal(
+                0, 1
+            ).sample(inputs.shape)
+
+            # Full kernel
+            true_predictive_density = get_predictive_density_se_kernel(
+                se_kernel, test_inputs_sample, inputs, outputs
+            )
+
+            mercer_gp = build_mercer_gp(
+                kernel_args, order, mercer_basis, eigenvalue_generator
+            )
+            mercer_gp = mercer_gp_builder.build()
+            mercer_gp.add_data(inputs, outputs)
+            try:
+                mercer_predictive_density = mercer_gp.get_predictive_density(
+                    test_inputs_sample
+                )
+            except ValueError:
+                mercer_failures[i, j] = 1
+                print(
+                    "Failed to acquire Mercer predictive density for experiment {} ".format(
+                        (str(i), str(j))
+                    )
+                )
+                continue
+
+            favard_basis = (
+                OrthoBuilder(order)
+                .set_sample(inputs)
+                .set_weight_function(weight_function)
+                .get_orthonormal_basis()
+            )
+
+            # favard_gp = build_mercer_gp(
+            # kernel_args, order, favard_basis, eigenvalue_generator
+            # )
+            favard_gp = favard_gp_builder.set_basis(favard_basis).build()
+            favard_gp.add_data(inputs, outputs)
+            try:
+                favard_predictive_density = favard_gp.get_predictive_density(
+                    test_inputs_sample
+                )
+            except ValueError:
+                favard_failures[i, j] = 1
+                print(
+                    "Failed to acquire Favard predictive density for experiment {} ".format(
+                        (str(i), str(j))
+                    )
+                )
+                continue
+
+            # now calculate the predictive densities
+            mercer_kl = D.kl.kl_divergence(
+                mercer_predictive_density, true_predictive_density
+            )
+            favard_kl = D.kl.kl_divergence(
+                favard_predictive_density, true_predictive_density
+            )
+            print(colored(mercer_kl, "yellow"))
+            print(colored(favard_kl, "magenta"))
+            if mercer_kl > favard_kl:
+                print(colored("Favard is better!", "green"))
+            else:
+                print(colored("Favard is worse!", "red"))
+            kls[i, j, 0] = mercer_kl
+            kls[i, j, 1] = favard_kl
+    print("mercer failures:", mercer_failures.sum(dim=0) / experiment_count)
+    print("favard failures:", favard_failures.sum(dim=0) / experiment_count)
+    # breakpoint()
+    if save:
+        if normal_test_inputs:
+            torch.save(
+                kls,
+                "./experiment_data/kl_divergences_normal_test_inputs.pt",
+            )
+        else:
+            torch.save(
+                kls,
+                "./experiment_data/kl_divergences_mixture_test_inputs.pt",
+            )
